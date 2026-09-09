cee28596b403c967ecf909664c938b2972cf239f
masnamsta <abderemane.faycal@hotmail.fr>
2026-09-09 10:40:37 +0200

    Initial skeleton: structure site vitrine cabinet d'avocat

---
+++ assets/js/contact-form.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-form')?.addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Formulaire prêt à être connecté à un service d\'envoi (Formspree ou backend).');
+});
---
+++ assets/js/main.js
@@ -0,0 +1,1 @@
+console.log('Site cabinet Abderemane chargé.');
